9358cf840b3c3591af3ac9673120360ceaeacae4
Jason Ballantyne <72609901+JasonBallantyne@users.noreply.github.com>
2022-01-09 22:31:05 +0000

    Delete 2.SQL directory

--- 2.SQL/Q2_SQLHeight.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Part 1.2 Q2
-# Script that selects the country & average height, groups by country and ordered by average height ascending
-
-mysql << EOF
-use People_Info;
-Select Country, avg(Height) as AverageHeight from Detailed_Description Group by Country Order by AverageHeight ASC;
-
-EOF
